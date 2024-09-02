2dd833f2e6ff4306c03031aac850ef99b85faf22
Vallail-dev <vallail.naqvi@v2solutions.com>
2024-09-02 10:47:42 +0530

    Update VerifyRolesAndPermissions.spec.ts

--- tests/RolesAndPermissionsTest/VerifyRolesAndPermissions.spec.ts
+++ tests/RolesAndPermissionsTest/VerifyRolesAndPermissions.spec.ts
@@ -4,29 +4,27 @@ import { RolesAndPermissionsPage } from "../../Pages/RolesAndPermissionsPage";
 import { HomePage } from "../../Pages/HomePage";
 import { Url, EmailAddress, Password } from "../../utils/config-utils";
 
-test("verify login of a user and retrieve all roles from table in roles and permissions tab", async ({
-  page,
-}) => {
+test("Roles and Permissions Tab functionality", async ({ page }) => {
   const loginPage = new LoginPage(page);
   const homePage = new HomePage(page);
   const rolesAndPermissions = new RolesAndPermissionsPage(page);
 
   // Login using email address and password
-  await test.step(`Login using email address and password`, async () => {
+  await test.step("Login using email address and password", async () => {
     await loginPage.login(Url, EmailAddress, Password);
   });
 
   // Go to Admin Portal
-  await test.step(`Go to Admin Portal Customer tab`, async () => {
+  await test.step("Go to Admin Portal Customer tab", async () => {
     await homePage.clickOnGoToAdminPortal();
   });
 
   // Verify default display of roles and permissions tab
-  await test.step("Verify Roles and Permissions tab", async () => {
+  await test.step("Verify roles in Roles and Permissions tab", async () => {
     await rolesAndPermissions.clickOnRolesAndPermissionsTab();
 
     const roles = await rolesAndPermissions.getAllRoles();
-    await expect(roles).toBeTruthy();
+    expect(roles).toBeTruthy();
   });
 
   // Verify URL of the roles and permissions tab
@@ -38,5 +36,10 @@ test("verify login of a user and retrieve all roles from table in roles and perm
   await test.step("Verify search by role in searchbar", async () => {
     await rolesAndPermissions.clickOnSearchBar();
     await rolesAndPermissions.isRoleDisplayed("admin");
+  });
+  //Verify search by partial role in search bar
+  await test.step("Verify search by Partial role in search bar ", async () => {
+    await rolesAndPermissions.clickOnSearchBar();
+    await rolesAndPermissions.isRoleDisplayed("Admi");
   });
 });
