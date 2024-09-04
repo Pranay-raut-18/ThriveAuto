7a65a9a5142b3017c4ed3117cdb8aed8b835226b
Vallail-dev <vallail.naqvi@v2solutions.com>
2024-09-04 15:29:36 +0530

    newest update 7 test cases working accordingly

--- tests/LoginPageTest/VerifyandGoingtoadminportalwithcorrectIDandPassword01.spec.ts
+++ tests/LoginPageTest/VerifyandGoingtoadminportalwithcorrectIDandPassword01.spec.ts
@@ -1,40 +1,44 @@
-import {test, expect} from '@playwright/test';
-import {LoginPage} from "../../Pages/LoginPage";
-import { UserPage } from '../../Pages/UserPage';
-import {HomePage} from "../../Pages/HomePage";
-import {Url, EmailAddress, Password } from "../../utils/config-utils"
+import { test, expect } from "@playwright/test";
+import { LoginPage } from "../../Pages/LoginPage";
+import { UserPage } from "../../Pages/UserPage";
+import { HomePage } from "../../Pages/HomePage";
+import { Url, EmailAddress, Password } from "../../utils/config-utils";
 
-test("Verify and Going to admin Portal with correct ID and Password TC-01",async({page})=>{
-    const loginPage = new LoginPage(page);             
-    const userPage = new UserPage(page);       
-    const homePage = new HomePage(page);       
+test("Verify and Going to admin Portal with correct ID and Password TC-01", async ({
+  page,
+}) => {
+  const loginPage = new LoginPage(page);
+  const userPage = new UserPage(page);
+  const homePage = new HomePage(page);
 
-    //Login using email address and password
-    await test.step(`Login using email address and password`, async () => {
-      await loginPage.login(Url, EmailAddress, Password);    
-    });
+  //Login using email address and password
+  await test.step(`Login using email address and password`, async () => {
+    await loginPage.login(Url, EmailAddress, Password);
+  });
 
-    //Verify user is logged in successfully
-    await test.step(`Verify user is logged in successfully`, async () => {
-      homePage.clickOnOpenAccountMenu();
-      await expect(await page.getByText('Log Out')).toHaveText("Log Out");
-      //change
-      await page.locator('#account-menu > .MuiBackdrop-root').click();
-      await page.waitForSelector("//p[@class='MuiTypography-root MuiTypography-body1 MuiTypography-noWrap MuiListItemText-primary css-gnfns7']")
-    })
+  //Verify user is logged in successfully
+  await test.step(`Verify user is logged in successfully`, async () => {
+    homePage.clickOnOpenAccountMenu();
+    await expect(await page.getByText("Log Out")).toHaveText("Log Out");
+    //change
+    await page.locator("#account-menu > .MuiBackdrop-root").click();
+    await page.waitForSelector(
+      "//p[@class='MuiTypography-root MuiTypography-body1 MuiTypography-noWrap MuiListItemText-primary css-gnfns7']"
+    );
+  });
 
-    //Go to Admin Potal Customer tab
-    await test.step(`Go to Admin Potal Customer tab`, async () => {
-      await homePage.clickOnGoToAdminPortal();
-      await expect(page).toHaveURL("https://thrive.thrive-dev.com/admin/users")
-    })
-    
-    //Click on Customer tab
-    await test.step(`Click Customer tab`, async () => {
-      await userPage.ClickOnCustomerTab();
-      await expect(page).toHaveURL("https://thrive.thrive-dev.com/admin/customers");
-      })
-    
-  
-  
-  })
+  //Go to Admin Potal Customer tab
+  await test.step(`Go to Admin Potal Customer tab`, async () => {
+    await homePage.clickOnGoToAdminPortal();
+    await expect(page).toHaveURL("https://thrive.thrive-dev.com/admin/users");
+  });
+
+  //Click on Customer tab
+  await test.step(`Click Customer tab`, async () => {
+    await userPage.ClickOnCustomerTab();
+    await expect(page).toHaveURL(
+      "https://thrive.thrive-dev.com/admin/customers"
+    );
+  });
+  await page.pause();
+});
