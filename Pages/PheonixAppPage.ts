c2e21fc6296f235a5046b0fe8f2bcc259678d09e
Vallail-dev <vallail.naqvi@v2solutions.com>
2024-09-19 12:54:29 +0530

    updated pheonix

---
+++ Pages/PheonixAppPage.ts
@@ -0,0 +1,12 @@
+import { Locator, Page } from "@playwright/test";
+
+/**
+ *  RolesAndPermissionsPage page
+ * @author Vallail N
+ */
+
+export class PheonixAppPage {
+  private start: Locator;
+
+  constructor(page: Page) {}
+}
