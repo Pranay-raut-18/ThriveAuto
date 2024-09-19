689e2bb0160575e4f0f3f623cb50781497efaf07
Pranay-raut-18 <163234422+Pranay-raut-18@users.noreply.github.com>
2024-09-19 14:30:29 +0530

    config file changes done

--- utils/config-utils.ts
+++ utils/config-utils.ts
@@ -1,10 +1,3 @@
-<<<<<<< Updated upstream
-
-export const Url = 'https://thrive.thrive-qa.com/admin/users'; 
-export const EmailAddress = 'qatesting+mk.12@thrivetrm.com';
-export const Password = 'Changeme1$';
-=======
 export const Url = "https://thrive.thrive-qa.com/admin/users";
 export const EmailAddress = "qatesting+mk.12@thrivetrm.com";
 export const Password = "Changeme1$";
->>>>>>> Stashed changes
