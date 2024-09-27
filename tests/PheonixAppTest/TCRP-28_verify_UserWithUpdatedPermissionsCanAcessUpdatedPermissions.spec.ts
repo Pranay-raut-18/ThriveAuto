0eb93afba303e838f626c3af64129e88094a9f07
Vallail-dev <vallail.naqvi@v2solutions.com>
2024-09-27 15:54:39 +0530

    latest changes 27 working

--- tests/PheonixAppTest/TCRP-28_verify_UserWithUpdatedPermissionsCanAcessUpdatedPermissions.spec.ts
+++ tests/PheonixAppTest/TCRP-28_verify_UserWithUpdatedPermissionsCanAcessUpdatedPermissions.spec.ts
@@ -1,18 +1,84 @@
 import { test, expect } from "@playwright/test";
 import { LoginPage } from "../../Pages/LoginPage";
-import { Url, EmailAddress1, Password1 } from "../../utils/config-utils";
+import {
+  Url,
+  EmailAddress1,
+  Password1,
+  EmailAddress,
+  Password,
+} from "../../utils/config-utils";
 import { PheonixAppPage } from "../../Pages/PheonixAppPage";
+import { getCompleteTimestamp } from "../../utils/common-utils";
+import { HomePage } from "../../Pages/HomePage";
+import { RolesAndPermissionsPage } from "../../Pages/RolesAndPermissionsPage";
 
-test("To Verify that the System role with view functionality is able to view people in the Pheonix Appplication", async ({
+test("To Verify that the System role with updated view functionality is able to view people in the Pheonix Appplication", async ({
   page,
 }) => {
   const loginPage = new LoginPage(page);
   const pheonixAppPage = new PheonixAppPage(page);
+  const homePage = new HomePage(page);
+  const rolesAndPermissions = new RolesAndPermissionsPage(page);
   const tabtoclick: string = "People";
   const persontosearch: string = "佐藤昌宏";
+  let timestamp: string;
+  timestamp = getCompleteTimestamp();
+  const RoleNametoEdit: string = "Only Tag View Role";
+  const Description: string = `AutoDescription${timestamp}`;
 
   // Login using email address and password
   await test.step("Login using email address and password", async () => {
+    await loginPage.login(Url, EmailAddress, Password);
+  });
+
+  // Go to Admin Portal
+  await test.step("Go to Admin Portal Customer tab", async () => {
+    await homePage.clickOnGoToAdminPortal();
+  });
+
+  // Clicks on Roles and Permissions Tab
+  await test.step("Click on Roles and Permissions Tab", async () => {
+    await rolesAndPermissions.clickOnRolesAndPermissionsTab();
+  });
+
+  //Clicks on search bar to search for the role to be edited
+  await test.step("Click on Search and search for the role to be edited", async () => {
+    await rolesAndPermissions.clickOnSearchBar();
+    await rolesAndPermissions.searchForRole(RoleNametoEdit);
+  });
+  //Click on menu item for rolename to edit
+  await test.step("Click on the action menu for the searched role", async () => {
+    const roleRow = page.locator(
+      `.MuiDataGrid-row:has-text("${RoleNametoEdit}")`
+    );
+    await roleRow
+      .locator('button[aria-label="Open roles action menu"]')
+      .click();
+  });
+
+  // Click on "Edit" menu item
+  await test.step("Click on 'Edit' menu item", async () => {
+    await rolesAndPermissions.clickOnMenuItem("Edit Details");
+  });
+
+  // Fill name and description
+  await test.step("Fill name,description and permissions", async () => {
+    await page.waitForLoadState("networkidle");
+    await rolesAndPermissions.fillRoleAndDescription(
+      RoleNametoEdit,
+      Description
+    );
+    await rolesAndPermissions.setPermission("person", "delete", true);
+    await rolesAndPermissions.saveChanges();
+    await page.waitForLoadState("networkidle");
+    await page.getByLabel("Open Account Menu").click();
+    await page.getByRole("menuitem", { name: "Log Out" }).click();
+    await page.waitForURL("https://thrive.thrive-qa.com/login");
+  });
+
+  // Login using email address and password
+  await test.step("Login using email address and password", async () => {
+    await page.waitForLoadState("networkidle");
     await loginPage.login(Url, EmailAddress1, Password1);
   });
   //Clicks on People tab in pheonix application
